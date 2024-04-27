00430af04b57637d1f26cf37ce32ddd6e23c9d99
Alexey Loskutov <loskutov@iMac-3828.local>
2024-04-27 16:48:47 +0300

    Add enverement

--- src/environment.tsx
+++ src/environment.tsx
@@ -3,5 +3,17 @@ import { Environment as EnvironmentBase } from "@react-three/drei";
 import hdri from "./hdri.hdr";
 
 export const Environment = () => {
-  return <EnvironmentBase files={hdri} />;
+  return <Environment
+  background={false} // can be true, false or "only" (which only sets the background) (default: false)
+  backgroundBlurriness={0} // optional blur factor between 0 and 1 (default: 0, only works with three 0.146 and up)
+  backgroundIntensity={1} // optional intensity factor (default: 1, only works with three 0.163 and up)
+  backgroundRotation={[0, Math.PI / 2, 0]} // optional rotation (default: 0, only works with three 0.163 and up)
+  environmentIntensity={1} // optional intensity factor (default: 1, only works with three 0.163 and up)
+  environmentRotation={[0, Math.PI / 2, 0]} // optional rotation (default: 0, only works with three 0.163 and up)
+  files={hdri}
+  path="/"
+  preset={null}
+  scene={undefined} // adds the ability to pass a custom THREE.Scene, can also be a ref
+  encoding={undefined} // adds the ability to pass a custom THREE.TextureEncoding (default: THREE.sRGBEncoding for an array of files and THREE.LinearEncoding for a single texture)
+/>
 };
