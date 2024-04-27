6f972fa5214d90e7b1c692a58b5f58f96105cc6c
Alexey Loskutov <loskutov@iMac-3828.local>
2024-04-27 17:25:46 +0300

    edit bg

--- src/background.tsx
+++ src/background.tsx
@@ -1,7 +1,7 @@
 import { useControls } from "leva";
 
 export function BackgroundColor() {
-  const { backgroundColor } = useControls({ backgroundColor: "#11202F" });
+  const { backgroundColor } = useControls({ backgroundColor: "#fff" });
 
   return <color attach="background" args={[backgroundColor]} />;
 }
