3a0caf1019c995d303b8a635c882429bae2cbf7a
Alexey Loskutov <loskutov@iMac-3828.local>
2024-04-27 17:15:06 +0300

    Add enverement setting

--- src/environment.tsx
+++ src/environment.tsx
@@ -5,11 +5,11 @@ import hdri from "./hdri.hdr";
 export const Environment = () => {
   return <Environment
   background={false} // can be true, false or "only" (which only sets the background) (default: false)
-  backgroundBlurriness={0} // optional blur factor between 0 and 1 (default: 0, only works with three 0.146 and up)
-  backgroundIntensity={1} // optional intensity factor (default: 1, only works with three 0.163 and up)
+  backgroundBlurriness={0.2} // optional blur factor between 0 and 1 (default: 0, only works with three 0.146 and up)
+  backgroundIntensity={2.5} // optional intensity factor (default: 1, only works with three 0.163 and up)
   backgroundRotation={[0, Math.PI / 2, 0]} // optional rotation (default: 0, only works with three 0.163 and up)
-  environmentIntensity={1} // optional intensity factor (default: 1, only works with three 0.163 and up)
-  environmentRotation={[0, Math.PI / 2, 0]} // optional rotation (default: 0, only works with three 0.163 and up)
+  environmentIntensity={2.5} // optional intensity factor (default: 1, only works with three 0.163 and up)
+  environmentRotation={[100, Math.PI / 2, 0]} // optional rotation (default: 0, only works with three 0.163 and up)
   files={hdri}
   path="/"
   preset={null}
