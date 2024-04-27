a81a4ccd49bd082927654e623e49fff32fcb65ed
Alexey Loskutov <loskutov@iMac-3828.local>
2024-04-27 17:42:17 +0300

    add ev intensity

--- src/environment.tsx
+++ src/environment.tsx
@@ -3,5 +3,7 @@ import { Environment as EnvironmentBase } from "@react-three/drei";
 import hdri from "./hdri.hdr";
 
 export const Environment = () => {
-  return <EnvironmentBase files={hdri} />;
+  return <EnvironmentBase 
+  files={hdri}
+  environmentIntensity={2} />;
 };
