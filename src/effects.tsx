673dd9a2ed4a05a684fc14829161b2c60a3e382a
Alexey Loskutov <loskutov@iMac-3828.local>
2024-04-27 20:18:43 +0300

    add godRays

--- src/effects.tsx
+++ src/effects.tsx
@@ -6,6 +6,7 @@ import {
   HueSaturation,
   SMAA,
   ToneMapping,
+  GodRays,
 } from "@react-three/postprocessing";
 import { folder, useControls } from "leva";
 import { BlendFunction } from "postprocessing";
@@ -142,11 +143,26 @@ export function Effects() {
           averageLuminance={toneMappingAverageLuminance}
           adaptationRate={toneMappingAdaptationRate}
         />
-       
         <Bloom intensity={bloomIntensity} mipmapBlur={mipmapBlur} />
 
         <>{antialiasing === "smaa" && <SMAA />}</>
       </EffectComposer>
+
+      <GodRays
+    sun={sunRef}
+    blendFunction={BlendFunction.Screen} // The blend function of this effect.
+    samples={60} // The number of samples per pixel.
+    density={0.96} // The density of the light rays.
+    decay={0.9} // An illumination decay factor.
+    weight={0.4} // A light ray weight factor.
+    exposure={0.6} // A constant attenuation coefficient.
+    clampMax={1} // An upper bound for the saturation of the overall effect.
+    width={Resizer.AUTO_SIZE} // Render width.
+    height={Resizer.AUTO_SIZE} // Render height.
+    kernelSize={KernelSize.SMALL} // The blur kernel size. Has no effect if blur is disabled.
+    blur={true} // Whether the god rays should be blurred to reduce artifacts.
+  />
+
     </Suspense>
   );
 }
