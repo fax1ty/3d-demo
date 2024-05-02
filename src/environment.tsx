2956db96720e7e06c2448ce123ca5235366a0f99
Алексей Кандинский <kandinsky@MacBook-Air.local>
2024-05-02 19:00:19 +0300

    Edit hdri

--- src/environment.tsx
+++ src/environment.tsx
@@ -3,7 +3,5 @@ import { Environment as EnvironmentBase } from "@react-three/drei";
 import hdri from "./hdri.hdr";
 
 export const Environment = () => {
-  return <EnvironmentBase 
-  files={hdri}
-  environmentIntensity={2} />;
+  return <EnvironmentBase files={hdri} environmentIntensity={3} />;
 };
