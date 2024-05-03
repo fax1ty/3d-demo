af45ecfd890d537bf88d37c533d235bd7c9f9594
Алексей Кандинский <kandinsky@MacBook-Air.local>
2024-05-03 17:45:50 +0300

    Edit light

--- src/environment.tsx
+++ src/environment.tsx
@@ -4,10 +4,7 @@ import hdri from "./hdri.hdr";
 
 export const Environment = () => {
   return (
-    <EnvironmentBase
-      files={hdri}
-      environmentIntensity={0.5}
-      environmentRotation={[45, Math.PI / 2, 0]}
-    />
+    <EnvironmentBase files={hdri} environmentIntensity={0.5} environmentRotation={[45, Math.PI / 2, 0]} />
+    <ContactShadows renderOrder={2} frames={1} resolution={1024} scale={120} blur={2} opacity={0.6} far={100} />
   );
 };
