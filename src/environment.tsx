6e6ec1cfd6b705e03a39014ead9746f0e8ed3cbb
Алексей Кандинский <kandinsky@MacBook-Air.local>
2024-05-03 10:58:30 +0300

    Edit effector

--- src/environment.tsx
+++ src/environment.tsx
@@ -3,5 +3,11 @@ import { Environment as EnvironmentBase } from "@react-three/drei";
 import hdri from "./hdri.hdr";
 
 export const Environment = () => {
-  return <EnvironmentBase files={hdri} environmentIntensity={1.2} />;
+  return (
+    <EnvironmentBase
+      files={hdri}
+      environmentIntensity={1.2}
+      environmentRotation={[90, Math.PI / 2, 0]}
+    />
+  );
 };
