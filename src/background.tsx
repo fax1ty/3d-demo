04ee09dd92a023903d330608c6708c8b047a4cca
Алексей Кандинский <kandinsky@MacBook-Air.local>
2024-05-03 10:41:52 +0300

    Edit effector

--- src/background.tsx
+++ src/background.tsx
@@ -1,7 +1,7 @@
 import { useControls } from "leva";
 
 export function BackgroundColor() {
-  const { backgroundColor } = useControls({ backgroundColor: "#2a4c6f" });
+  const { backgroundColor } = useControls({ backgroundColor: "#0D1827" });
 
   return <color attach="background" args={[backgroundColor]} />;
 }
